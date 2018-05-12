1e1e1358d32cd82aeb20f0c7595919e39ac80cb3
miles <miles@glowing.com>
2018-05-12 17:59:30 +0800

    add apollo client

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+import { ApolloProvider } from 'react-apollo';
+import { ExchangeRates } from './components/ExchangeRates'
 
+const client = new ApolloClient({
+  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+})
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            My first Apollo App🚀
+          </p>
+          <ExchangeRates />
+        </div>
+      </ApolloProvider>
     );
   }
 }
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -4,5 +4,19 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+// client
+//   .query({
+//     query: gql`
+//     {
+//       rates(currency: "USD") {
+//         currency
+//       }
+//     }
+//     `
+//   })
+//   .then(r => {
+//     console.warn(r);
+//   })
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
